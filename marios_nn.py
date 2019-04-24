3815f1da4376385caa557f964b13c26cc22621d4
Nicholas Barrett <nbarrett@unm.edu>
2019-04-24 02:33:07 -0600

    fixed gui issues and messed with nn and ga settings

--- marios_nn.py
+++ marios_nn.py
@@ -10,13 +10,7 @@ from multiprocessing import Pool
 from multiprocessing.dummy import Pool as Pool2
 
 mario_height = 70
-mario_width = 50
-mario_max_x_vel = 500#250
-mario_y_up_accel = 500
-mario_y_down_accel = 50
-mario_x_accel = 20
-num_of_marios = 150
-mario_defeat_bounce = .75
+mario_width = 55
 mario_right_sprites = [pygame.image.load(
             'sprites\\mario\\mario-right\\mario'+str(i+1)+'.png') for i in range(6)]
 mario_right_sprites = [pygame.transform.scale(mario_right_sprites[i],
@@ -42,6 +36,15 @@ luigi_right_jump = pygame.transform.scale(luigi_right_jump,(mario_width,mario_he
 luigi_left_jump = pygame.image.load('sprites\\luigi\\luigi-left\\luigijump.png')
 luigi_left_jump = pygame.transform.scale(luigi_left_jump,(mario_width,mario_height))
 
+
+mario_max_x_vel = 500#250
+mario_y_up_accel = 500
+mario_y_down_accel = 50
+mario_x_accel = 20
+num_of_marios = 200
+mario_defeat_bounce = .75
+
+
 player = False
 displayArena = False
 displayBest = True
@@ -50,26 +53,27 @@ save_gens = True
 fps = 60
 gravity = 750
 x_vel_slow = 1 - 0.8 / fps
-x_vel_round_digit = 10
+x_vel_round_digit = 2
 
-arena_len = 1000
-arena_height = 500
+arena_len = 1500
+arena_height = 700
 arena_floor = 1
-arena_leftwall = 1
-arena_rightwall = arena_leftwall + arena_len - mario_width
+arena_leftwall = 40
+arena_rightwall = arena_len - mario_width
 arena_max_duration = 1500
 arena_move_polling_rate = 15
 ground_width = 40
-random_y_max = mario_height*3
+random_y_max = mario_height*2
 ground_color = (139,69,19)
 ground_rect = pygame.Rect(0,
         arena_height,
         arena_len,
         ground_width)
+
 background_color = (0,108,170)
 
 num_of_input_nodes = 12
-num_of_layer1_nodes = 20
+num_of_layer1_nodes = 14
 num_of_layer2_nodes = 6
 mario_nn_layer1 = np.array([[[random.random()*2 - 1 for i in range(num_of_input_nodes)]
                                 for i2 in range(num_of_layer1_nodes)]
@@ -77,11 +81,12 @@ mario_nn_layer1 = np.array([[[random.random()*2 - 1 for i in range(num_of_input_
 mario_nn_layer2 = np.array([[[random.random()*2 - 1 for i in range(num_of_layer1_nodes)]
                                 for i2 in range(num_of_layer2_nodes)]
                                     for i3 in range(num_of_marios)])
-mutation_prob = 3/(num_of_layer1_nodes + num_of_layer2_nodes)
+mutation_prob = 6/(num_of_layer1_nodes + num_of_layer2_nodes)
+crossover_probability = 0.4
 num_of_tourn_select = 15
-num_of_best_to_select = 20
-num_of_gen = 50
-num_of_matches = 5
+num_of_best_to_select = 30
+num_of_gen = 100
+num_of_matches = 3
 jump_threshold = 300
 move_threshold = 300
 
@@ -118,6 +123,10 @@ def mario_fight(marios):
         if(displayArena == True):
             display.fill(background_color)
             pygame.draw.rect(display,ground_color,ground_rect,0)
+            pygame.draw.rect(display,(40,160,40),pygame.Rect(0,
+                    0, arena_leftwall, arena_height),0)
+            pygame.draw.rect(display,(40,160,40),pygame.Rect(arena_rightwall+mario_width,
+                    0, 1000, arena_height),0)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 stop = True
@@ -145,10 +154,10 @@ def mario_fight(marios):
                 mario_y_vel[i] -= gravity/fps
             if(mario_xs[i] < arena_leftwall):
                 mario_xs[i] = arena_leftwall
-                mario_x_vel[i] = 0
+                mario_x_vel[i] = -1
             elif(mario_xs[i] > arena_rightwall):
                 mario_xs[i] = arena_rightwall
-                mario_x_vel[i] = 0
+                mario_x_vel[i] = 1
             if(new_mario_ys[i] < arena_floor):
                 new_mario_ys[i] = arena_floor
                 mario_y_vel[i] = 0
@@ -252,7 +261,9 @@ def display_mario(step, marioID, x, y, xvel, yvel):
         else:
             display.blit(mario_left_jump,(x,arena_height - y - mario_height))
     else:
-        if(xvel >= 0):
+        if(round(xvel) == 0):
+            display.blit(mario_right_sprites[0],(x,arena_height - y - mario_height))
+        elif(xvel > 0):
             display.blit(mario_right_sprites[step%6],(x,arena_height - y - mario_height))
         else:
             display.blit(mario_left_sprites[step%6],(x,arena_height - y - mario_height))
@@ -265,8 +276,10 @@ def display_luigi(step, marioID, x, y, xvel, yvel):
         else:
             display.blit(luigi_left_jump,(x,arena_height - y - mario_height))
     else:
-        if(xvel >= 0):
-            display.blit(luigi_right_sprites[step%6],(x,arena_height - y - mario_height))
+        if(round(xvel) == 0):
+            display.blit(mario_right_sprites[0],(x,arena_height - y - mario_height))
+        elif(xvel > 0):
+            display.blit(mario_right_sprites[step%6],(x,arena_height - y - mario_height))
         else:
             display.blit(luigi_left_sprites[step%6],(x,arena_height - y - mario_height))
 
@@ -287,6 +300,11 @@ def crossover(best):
     global mario_nn_layer2
     for i in range(num_of_marios):
         if(i in best): continue
+        if(random.random() < crossover_probability):
+            r = random.sample(best,1)[0]
+            mario_nn_layer1[i] = np.copy(mario_nn_layer1[r])
+            mario_nn_layer2[i] = np.copy(mario_nn_layer2[r])
+            continue
         best_2 = random.sample(best,2)
         cx_point = random.randint(1,num_of_layer1_nodes + num_of_layer2_nodes-1)
         if(cx_point < num_of_layer1_nodes):
@@ -306,6 +324,9 @@ def genetic_algorithm():
     global fps
     global arena_max_duration
     global arena_move_polling_rate
+    global arena_len
+    global arena_rightwall
+    global arena_leftwall
     path = "best_nn"
     try:
         os.mkdir(path)
@@ -317,6 +338,9 @@ def genetic_algorithm():
         fps = 20
         arena_max_duration = 100*fps
         arena_move_polling_rate = 3
+        rand = (arena_len - random.randint(700,1480))/2
+        arena_rightwall = arena_len - rand - mario_width
+        arena_leftwall = rand
         print("gen: "+str(gen))
         if(stop == True): break
         best = [0]*num_of_best_to_select
@@ -335,15 +359,22 @@ def genetic_algorithm():
             best[i] = IDs[num_of_tourn_select-1]
             scores[i] = results[num_of_tourn_select-1]
         best = numpy.unique(best).tolist()
+        scores , best = (list(l) for l in zip(*sorted(zip(scores , best))))
+        best = [best[i] for i in range(len(best)) if scores[i] != 0 ]
+        scores = [scores[i] for i in range(len(scores)) if scores[i] != 0 ]
+        if(len(best) < 2): best = random.sample(range(0,num_of_marios),num_of_tourn_select)
         print("Num of Unique Best:  "+ str(len(best)))
         print("Average Defeated Marios:  "+ str(np.mean(scores)/num_of_matches)+"\n")
         if(displayBest == True):
             displayArena = True
             fps = 30
             arena_max_duration = 750
-            arena_move_polling_rate = 1
+            arena_move_polling_rate = 3
             #player = True
-            mario_fight(best)
+            if(len(best) < num_of_tourn_select):
+                mario_fight(best)
+            else:
+                mario_fight(best[len(best)-num_of_tourn_select:len(best)])
             display.fill(background_color)
             pygame.draw.rect(display,ground_color,ground_rect,0)
             #player = False
